89b33cb383b10762119d58dd3cefbd2dc2dea3cc
saravanabalajidevaraj <47107103+saravanabalajidevaraj@users.noreply.github.com>
2021-05-19 08:30:02 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
- // apiUrl: 'http://128.199.210.214:4000/api/v1'
- apiUrl:'http://168.119.159.183:4300'
+  apiUrl: 'http://128.199.210.214:4000/api/v1'
+ //apiUrl:'http://168.119.159.183:4300'
  //apiUrl:'http://lss-project.herokuapp.com'
 };
